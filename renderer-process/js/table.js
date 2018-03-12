22083be625cb27e661ee6977bfe7a858708c6e52
Yudong Cao <>
2018-03-12 13:51:35 +0800

    date picker

--- renderer-process/js/table.js
+++ renderer-process/js/table.js
@@ -6,7 +6,7 @@ const newWindowBtn = document.getElementById('new')
 
 ipcRenderer.on('add-new-loan', (event, arg) => {
   const message = `Asynchronous message reply: ${arg}`
-  document.getElementById('data').innerHTML += "<tr><td>1</td><td>A001</td><td>Sachin</td><td>53023421532542345</td><td>62212424245345345345</td><td>2018-01-01</td><td>2019-01-01</td><td>10000.00</td><td>630</td><td>27.18</td><td>602.82</td><td>141.95</td><td>153.62</td><td>153.62</td><td>153.62</td></tr>";
+  document.getElementById('data').innerHTML += "<tr><td>1</td><td>A001</td><td>Sachin</td><td>53023421532542345</td><td>62212424245345345345</td><td>2018-01-01</td><td>2019-01-01</td><td>10000.00</td><td>630</td><td>27.18</td><td>602.82</td><td>运输公司</td><td>141.95</td><td>153.62</td><td>153.62</td><td>153.62</td></tr>";
 })
 
 
