56829cdd53f6eac19818b65a280499525139398e
Yudong Cao <cydjohn@live.com>
2018-03-23 23:19:55 -0400

    update

--- renderer-process/js/table.js
+++ renderer-process/js/table.js
@@ -3,36 +3,7 @@ const path = require('path')
 const { ipcRenderer } = require('electron')
 // const newWindowBtn = document.getElementById('new')
 
-let allData = [{
-  name:"cyd",
-  contractNumber:"A0001",
-  idNumber:"530453199408140920",
-  bankAccount:"6222342109485743",
-  startTime:"2018-3-20",
-  endTime:"2019-3-20",
-  amount:"10000",
-  interestRate:"6.3",
-  tax:"27.18",
-  actualInterest:"602.82",
-  firstPayment:"141.95",
-  restPayment:"153.63",
-  interest:"630"
-},
-{
-  name:"zxh",
-  contractNumber:"A0002",
-  idNumber:"524453199408140920",
-  bankAccount:"6222432109485743",
-  startTime:"2018-3-20",
-  endTime:"2019-3-20",
-  amount:"10000",
-  interestRate:"6.3",
-  tax:"27.18",
-  actualInterest:"602.82",
-  firstPayment:"141.95",
-  restPayment:"153.63",
-  interest:"630"
-}]
+let allData = []
 
 let tableData = []
 // loadData()
