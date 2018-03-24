1a130d21f3ee93f7c872663e97ec7b9d9f0b8914
Yudong Cao <cydjohn@live.com>
2018-03-23 21:32:59 -0400

    add print preview

--- renderer-process/js/print-preview.js
+++ renderer-process/js/print-preview.js
@@ -2,6 +2,42 @@ const { BrowserWindow } = require('electron').remote
 const path = require('path')
 const { ipcRenderer } = require('electron')
 
+let tableData = []
 
 
+
+ipcRenderer.send('get-print-value')
+
+ipcRenderer.on('print-data', (event, data) => {
+  tableData = data
+  loadData()
+})
+
+
+function loadData() {
+  document.getElementById('data').innerHTML = ""
+  for (d in tableData) {
+    document.getElementById('data').innerHTML +=
+      "<tr>" +
+      "<td>" + tableData[d].contractNumber + "</td>" +
+      "<td>" + tableData[d].name + "</td>" +
+      "<td>" + tableData[d].bankAccount + "</td>" +
+      "<td>" + tableData[d].startTime + "</td>" +
+      "<td>" + tableData[d].actualInterest + "</td>" +
+      "</tr>"
+  }
+}
+
+
+
+const printPDFBtn = document.getElementById('print-pdf')
+
+printPDFBtn.addEventListener('click', (event) => {
+  printPDFBtn.hidden = true
+  ipcRenderer.send('print-to-pdf')
+})
+
+ipcRenderer.on('wrote-pdf', (event, path) => {
+  printPDFBtn.hidden = false
+})
 
--- renderer-process/js/table.js
+++ renderer-process/js/table.js
@@ -34,7 +34,7 @@ let allData = [{
   interest:"630"
 }]
 
-// let tableData = allData
+let tableData = []
 // loadData()
 
 
@@ -109,10 +109,14 @@ contraIdSearchBox.addEventListener("input",()=>{
 
 
 // 打印预览
+const printPreview = document.getElementById('print-preview')
 
+printPreview.addEventListener('click', (event) => {
+  ipcRenderer.send('pass-print-value',tableData)
+  const modalPath = path.join('file://', __dirname, '../../sections/windows/print-preview.html')
+  let win = new BrowserWindow({ width: 800, height: 1000 })
+  win.on('close', () => { win = null })
+  win.loadURL(modalPath)
+  win.show()
+})
 
-// const n = document.getElementById("new");
-// const e = document.getElementById("data");
-// n.addEventListener('click', (event) => {
-//     e.innerHTML += "<tr><td>1</td><td>A001</td><td>Sachin</td><td>53023421532542345</td><td>62212424245345345345</td><td>2018-01-01</td><td>2019-01-01</td><td>10000.00</td><td>630</td><td>27.18</td><td>602.82</td><td>141.95</td><td>153.62</td><td>153.62</td><td>153.62</td></tr>";
-// })
