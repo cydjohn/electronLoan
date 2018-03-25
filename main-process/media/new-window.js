07b2be6acf70632d956de0f526770e0a893fc088
Yudong Cao <cydjohn@live.com>
2018-03-24 21:02:50 -0400

    update

--- main-process/media/new-window.js
+++ main-process/media/new-window.js
@@ -1,27 +1,29 @@
 const path = require('path')
-const { ipcMain, ipcRenderer,app} = require('electron')
+const { ipcMain, ipcRenderer, app } = require('electron')
 const { BrowserWindow } = require('electron')
 
 
 var Datastore = require('nedb')
 var userData = app.getAppPath('userData');
-db = new Datastore({ filename: userData+'/db/persons.db', autoload: true });
+db = new Datastore({ filename: userData + '/db/persons.db', autoload: true });
 
 
 let tableData = []
 let tempData = {}
 
 ipcMain.on('getMsg', (event, arg) => {
-    var arr = BrowserWindow.getAllWindows();
-    for (var i = 0; i < arr.length; i++) {
-        const toWindow = arr[i];
-        toWindow.webContents.send('add-new-loan', arg);
-    }
+
     db.insert(arg, function (err, newDoc) {   // Callback is optional
         // newDoc is the newly inserted document, including its _id
         // newDoc has no key called notToBeSaved since its value was undefined
+        var arr = BrowserWindow.getAllWindows();
+        for (var i = 0; i < arr.length; i++) {
+            const toWindow = arr[i];
+            toWindow.webContents.send('add-new-loan', newDoc);
+            tempData = newDoc
+        }
     });
-    tempData = arg
+
 
 })
 
--- renderer-process/js/table.js
+++ renderer-process/js/table.js
@@ -100,3 +100,5 @@ printPreview.addEventListener('click', (event) => {
 
 
 
+
+
