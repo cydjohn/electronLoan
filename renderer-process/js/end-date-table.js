536cc436b85620cc48926b999ac0509b3d41e828
Yudong Cao <cydjohn@live.com>
2018-05-05 17:09:13 -0400

    fix end-date-table print preview

--- renderer-process/js/end-date-table.js
+++ renderer-process/js/end-date-table.js
@@ -9,33 +9,12 @@ let tableData = []
 // loadData()
 
 ipcRenderer.send('request-all-data')
-
 ipcRenderer.on('get-all-data', (event, arg) => {
   allData = arg
 })
 
-ipcRenderer.on('add-new-loan', (event, arg) => {
-  allData.push(arg)
-  tableData = []
-  tableData.push(arg)
-  loadData()
-  document.getElementById('button-table').click()
-})
-
-ipcRenderer.on('delete-contract-number', (event, arg) => {
-  allData = allData.filter(function (item) {
-    return item.contractNumber !== arg
-  })
-  tableData = tableData.filter(function (item) {
-    return item.contractNumber !== arg
-  })
-  console.log("delete " + arg)
-  loadData()
-})
 
 function loadData() {
-    console.log(tableData)
-    console.log(allData)
   document.getElementById('end-date-table-data').innerHTML = ""
   for (d in tableData) {
     document.getElementById('end-date-table-data').innerHTML +=
@@ -57,27 +36,12 @@ function loadData() {
 }
 
 function calculateSum() {
-  var loanSum = 0, interestSum = 0, taxSum = 0, acturalInterestSum = 0, firstInterestSum = 0,
-    secondInterestSum = 0, thirdInterestSum = 0, fourthInterestSum = 0
+  var loanSum = 0;
   for (i in tableData) {
     loanSum += parseFloat(tableData[i].amount)
-    interestSum += parseFloat(tableData[i].interest)
-    taxSum += parseFloat(tableData[i].tax)
-    acturalInterestSum += parseFloat(tableData[i].actualInterest)
-    firstInterestSum += parseFloat(tableData[i].firstPayment)
-    secondInterestSum += parseFloat(tableData[i].restPayment)
-    thirdInterestSum += parseFloat(tableData[i].restPayment)
-    fourthInterestSum += parseFloat(tableData[i].restPayment)
   }
-
-  document.getElementById("loan-sum").innerHTML = loanSum
-  document.getElementById("interest-sum").innerHTML = interestSum
-  document.getElementById("tax-sum").innerHTML = taxSum
-  document.getElementById("actural-interest-sum").innerHTML = acturalInterestSum.toFixed(2)
-  document.getElementById("first-interest-sum").innerHTML = firstInterestSum.toFixed(2)
-  document.getElementById("second-interest-sum").innerHTML = secondInterestSum.toFixed(2)
-  document.getElementById("third-interest-sum").innerHTML = thirdInterestSum.toFixed(2)
-  document.getElementById("fourth-interest-sum").innerHTML = fourthInterestSum.toFixed(2)
+  document.getElementById("end-date-table-loan-sum").innerHTML = loanSum
+  console.log('sum',loanSum);
 }
 
 
@@ -115,7 +79,7 @@ contraIdSearchBox.addEventListener("input", () => {
 
 
 // 打印预览
-const printPreview = document.getElementById('print-preview')
+const printPreview = document.getElementById('end-date-table-print-preview')
 printPreview.addEventListener('click', (event) => {
   ipcRenderer.send('pass-print-value', tableData)
   const modalPath = path.join('file://', __dirname, '../../sections/windows/print-preview.html')
@@ -125,20 +89,8 @@ printPreview.addEventListener('click', (event) => {
   win.show()
 })
 
-// 删除
-
-const deleteRecorde = document.getElementById('delete')
-deleteRecorde.addEventListener('click', (event) => {
-  const modalPath = path.join('file://', __dirname, '../../sections/windows/delete.html')
-  let win = new BrowserWindow({ width: 600, height: 400 })
-  win.on('close', () => { win = null })
-  win.loadURL(modalPath)
-  win.show()
-})
-
-
 // 借款日期筛选
-const startDate = document.getElementById('datetimepicker1')
+const startDate = document.getElementById('end-date-table-datetimepicker1')
 function checkStartDate(idn) {
   if (startDate.value == "") {
     return false
@@ -153,7 +105,7 @@ startDate.addEventListener("input", (event, arg) => {
 
 
 // 还款日期筛选
-const endDate = document.getElementById('datetimepicker3')
+const endDate = document.getElementById('end-date-table-datetimepicker3')
 function checkEndDate(idn) {
   if (endDate.value == "") {
     return false
@@ -167,20 +119,6 @@ endDate.addEventListener("input", (event, arg) => {
 })
 
 
-// 付息日筛选
-const interestDate = document.getElementById('datetimepicker2')
-function checkInterestDate(idn) {
-  if (interestDate.value == "") {
-    return false
-  }
-  console.log(interestDate.value)
-  return idn.firstDay.search(interestDate.value) != -1
-}
-
-interestDate.addEventListener("input", (event, arg) => {
-  tableData = allData.filter(checkInterestDate);
-  loadData()
-})
 
 
 
