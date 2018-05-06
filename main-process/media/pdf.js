ee832286827bfa28cec0efb209e7d93224c70593
Yudong Cao <cydjohn@live.com>
2018-05-06 00:03:22 -0400

    update print page

--- main-process/media/pdf.js
+++ main-process/media/pdf.js
@@ -7,7 +7,7 @@ ipcMain.on('print-to-pdf', (event) => {
   const pdfPath = path.join(os.tmpdir(), 'print.pdf')
   const win = BrowserWindow.fromWebContents(event.sender)
   // Use default printing options
-  win.webContents.printToPDF({landscape: true,pageSize:"A3"}, (error, data) => {
+  win.webContents.printToPDF({landscape: false,pageSize:"A4"}, (error, data) => {
     if (error) throw error
     fs.writeFile(pdfPath, data, (error) => {
       if (error) throw error
