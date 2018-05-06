72c1101467f1021244159ba1e1fce59175c80f12
Yudong Cao <cydjohn@live.com>
2018-05-06 11:53:20 -0400

    update print

--- main-process/media/pdf.js
+++ main-process/media/pdf.js
@@ -3,11 +3,12 @@ const os = require('os')
 const path = require('path')
 const {BrowserWindow, ipcMain, shell} = require('electron')
 
-ipcMain.on('print-to-pdf', (event) => {
+ipcMain.on('print-to-pdf', (event,arg) => {
   const pdfPath = path.join(os.tmpdir(), 'print.pdf')
   const win = BrowserWindow.fromWebContents(event.sender)
   // Use default printing options
-  win.webContents.printToPDF({landscape: false,pageSize:"A4"}, (error, data) => {
+  win.webContents.printToPDF({landscape: true ,pageSize:"A4"}, (error, data) => {
+
     if (error) throw error
     fs.writeFile(pdfPath, data, (error) => {
       if (error) throw error
