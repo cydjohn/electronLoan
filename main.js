7986b395d17ff4470dd84456843142f6eb975150
cydjohn <cydjohn@live.com>
2018-05-12 01:35:26 -0400

    update

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ const debug = /--debug/.test(process.argv[2])
 
 var Datastore = require('nedb')
 var userData = app.getAppPath('userData');
-db = new Datastore({ filename: userData+'/.dist/persons.db'});
+// db = new Datastore({ filename: userData+'/.dist/persons.db'});
 
-// db = new Datastore({ filename: 'D://db/persons.db'});
+db = new Datastore({ filename: 'D://db/persons.db'});
 
 if (process.mas) app.setName('借款系统')
 
